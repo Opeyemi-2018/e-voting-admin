fa1bf0d55353322e890d237ffc578a5d0a907ed4
Opeyemi odunayo <127036369+Opeyemi-2018@users.noreply.github.com>
2025-08-25 13:33:13 +0100

    ui fix

--- app/components/overview/index.jsx
+++ app/components/overview/index.jsx
@@ -2,29 +2,30 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { IoPeopleOutline } from "react-icons/io5";
+import { ClipLoader } from "react-spinners"; // Import ClipLoader
 
 const Overview = () => {
   const [count, setCount] = useState(0);
   const [usedNumber, setUsedNumber] = useState(0);
   const [unusedNumber, setUnUsedNumber] = useState(0);
   const [candidateNumber, setcandidateNumber] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loadingCount, setLoadingCount] = useState(true);
+  const [loadingCandidates, setLoadingCandidates] = useState(true);
+  const [loadingVoters, setLoadingVoters] = useState(true);
 
   useEffect(() => {
     const fetchCount = async () => {
       try {
         const response = await axios.get(
-          // "https://e-voting-server-bxpt.onrender.com/api/unique-number/get-unique-number"
-          "http://localhost:5000/api/student-auth/students"
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/student-auth/students`
         );
         setCount(response.data.count);
       } catch (error) {
         console.error("Error fetching count:", error);
       } finally {
-        setLoading(false);
+        setLoadingCount(false);
       }
     };
-
     fetchCount();
   }, []);
 
@@ -32,21 +33,18 @@ const Overview = () => {
     const fetchCandidates = async () => {
       try {
         const res = await axios.get(
-          // "https://e-voting-server-bxpt.onrender.com/api/candidate/get-candidate"
-          "http://localhost:5000/api/candidate/get-candidate"
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/candidate/get-candidate`
         );
         setcandidateNumber(res.data);
       } catch (error) {
-        toast.error(error.response?.data?.msg || "Failed to fetch candidates");
+        console.error("Failed to fetch candidates:", error);
       } finally {
-        setLoading(false);
+        setLoadingCandidates(false);
       }
     };
-
     fetchCandidates();
   }, []);
 
-  // used number
   useEffect(() => {
     const fetchUsedNumbers = async () => {
       try {
@@ -54,25 +52,28 @@ const Overview = () => {
           `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/student-auth/voted-students`,
           { withCredentials: true }
         );
-
-        // setUsedNumbers(res.data.usedNumbers);
         setUsedNumber(res.data.votedCount);
         setUnUsedNumber(res.data.votedNonCount);
       } catch (err) {
         console.error(err.response?.data || err.message);
-        setError("Failed to load used numbers");
       } finally {
-        setLoading(false);
+        setLoadingVoters(false);
       }
     };
-
     fetchUsedNumbers();
   }, []);
+
+  const loaderStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "24px", // To match the height of the text
+  };
 
   return (
     <div>
       <div className="flex items-center gap-4 overflow-auto mt-6">
-        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] bg-white shadow-lg flex flex-col justify-between gap-[10px] h-[140px]  ">
+        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] bg-white shadow-lg flex flex-col justify-between gap-[10px] h-[140px]">
           <div className="flex items-center text-[#443227] justify-between">
             <p className="text-wrap">Total Registered Student</p>
             <IoPeopleOutline
@@ -80,19 +81,35 @@ const Overview = () => {
               className="bg-[#e1dfd3] p-1 rounded-full text-[#b72522]"
             />
           </div>
-          <p className="font-semibold text-2xl"> {count}</p>
+          <p className="font-semibold text-2xl">
+            {loadingCount ? (
+              <div style={loaderStyle}>
+                <ClipLoader size={20} color="#b72522" />
+              </div>
+            ) : (
+              count
+            )}
+          </p>
         </div>
-        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] shadow-lg bg-white flex flex-col justify-between gap-[10px] h-[140px]  ">
+        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] shadow-lg bg-white flex flex-col justify-between gap-[10px] h-[140px]">
           <div className="flex items-center text-[#443227] justify-between">
-            <p className="text-wrap">Total Registered candidate</p>
+            <p className="text-wrap">Total Registered Candidate</p>
             <IoPeopleOutline
               size={25}
               className="bg-[#e1dfd3] p-1 rounded-full text-[#b72522]"
             />
           </div>
-          <p className="font-semibold text-2xl">{candidateNumber.length}</p>
+          <p className="font-semibold text-2xl">
+            {loadingCandidates ? (
+              <div style={loaderStyle}>
+                <ClipLoader size={20} color="#b72522" />
+              </div>
+            ) : (
+              candidateNumber.length
+            )}
+          </p>
         </div>
-        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] shadow-lg bg-white flex flex-col justify-between gap-[10px] h-[140px]  ">
+        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] shadow-lg bg-white flex flex-col justify-between gap-[10px] h-[140px]">
           <div className="flex items-center text-[#443227] justify-between">
             <p>Total Voters</p>
             <IoPeopleOutline
@@ -100,9 +117,17 @@ const Overview = () => {
               className="bg-[#e1dfd3] p-1 rounded-full text-[#b72522]"
             />
           </div>
-          <p className="font-semibold text-2xl">{usedNumber}</p>
+          <p className="font-semibold text-2xl">
+            {loadingVoters ? (
+              <div style={loaderStyle}>
+                <ClipLoader size={20} color="#b72522" />
+              </div>
+            ) : (
+              usedNumber
+            )}
+          </p>
         </div>
-        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] shadow-lg bg-white flex flex-col justify-between gap-[10px] h-[140px]  ">
+        <div className="w-full text-nowrap min-w-[188px] p-4 rounded-[10px] shadow-lg bg-white flex flex-col justify-between gap-[10px] h-[140px]">
           <div className="flex items-center text-[#443227] justify-between">
             <p className="text-wrap">Total Non-Voters</p>
             <IoPeopleOutline
@@ -110,7 +135,15 @@ const Overview = () => {
               className="bg-[#e1dfd3] p-1 rounded-full text-[#b72522]"
             />
           </div>
-          <p className="font-semibold text-2xl">{unusedNumber}</p>
+          <p className="font-semibold text-2xl">
+            {loadingVoters ? (
+              <div style={loaderStyle}>
+                <ClipLoader size={20} color="#b72522" />
+              </div>
+            ) : (
+              unusedNumber
+            )}
+          </p>
         </div>
       </div>
     </div>
